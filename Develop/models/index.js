b46b4c64b4c698e52d12289aa3b22ceb38b98978
Keely Bailey <keelybailey3@gmail.com>
2021-08-04 14:45:51 -0500

    Add model indexes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+})
 
 module.exports = {
   Product,
